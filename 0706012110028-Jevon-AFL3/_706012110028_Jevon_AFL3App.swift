6ac9b84ad44645a6a3908857292e8ed44d1180ca
JevonCledwyn <jcledwyn@student.ciputra.ac.id>
2023-04-11 08:25:28 +0700

    Initial Commit

---
+++ 0706012110028-Jevon-AFL3/_706012110028_Jevon_AFL3App.swift
@@ -0,0 +1,17 @@
+//
+//  _706012110028_Jevon_AFL3App.swift
+//  0706012110028-Jevon-AFL3
+//
+//  Created by MacBook Pro on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _706012110028_Jevon_AFL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
