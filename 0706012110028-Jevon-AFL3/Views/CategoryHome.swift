3758f4bd807bb08f87a86b0370dc4dba4831c183
JevonCledwyn <jcledwyn@student.ciputra.ac.id>
2023-04-27 20:55:01 +0700

    Working with UI Controls - Section 1

--- 0706012110028-Jevon-AFL3/Views/CategoryHome.swift
+++ 0706012110028-Jevon-AFL3/Views/CategoryHome.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
+    @State private var showingProfile = false
 
     var body: some View {
         NavigationView {
@@ -25,7 +26,21 @@ struct CategoryHome: View {
                 }
                 .listRowInsets(EdgeInsets())
             }
+            // Add the listStyle modifier
+            .listStyle(.inset)
             .navigationTitle("Featured")
+            // add a user profile button
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                    Label("User Profile", systemImage: "person.crop.circle")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environmentObject(modelData)
+            }
         }
     }
 }
