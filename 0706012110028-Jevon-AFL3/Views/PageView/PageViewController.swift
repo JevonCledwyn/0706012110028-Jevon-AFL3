a205db6fb82a7f49be1c7e79b02068e9471a832a
JevonCledwyn <jcledwyn@student.ciputra.ac.id>
2023-04-27 23:31:52 +0700

    Interfacing with UIKit - section 2

--- 0706012110028-Jevon-AFL3/Views/PageView/PageViewController.swift
+++ 0706012110028-Jevon-AFL3/Views/PageView/PageViewController.swift
@@ -12,18 +12,61 @@ import UIKit
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
     
+    // another method to PageViewController
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
+        pageViewController.dataSource = context.coordinator
 
         return pageViewController
     }
     // calls setViewControllers
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+            // nitialize an array of controllers
+            [context.coordinator.controllers[0]], direction: .forward, animated: true)
     }
+    
+    // Declare a nested Coordinator
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        var parent: PageViewController
+        var controllers = [UIViewController]()
 
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
+        // Add UIPageViewControllerDataSource conformance
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerBefore viewController: UIViewController) -> UIViewController?
+        {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index == 0 {
+                return controllers.last
+            }
+            return controllers[index - 1]
+        }
+
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerAfter viewController: UIViewController) -> UIViewController?
+        {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index + 1 == controllers.count {
+                return controllers.first
+            }
+            return controllers[index + 1]
+        }
+    }
 }
 
