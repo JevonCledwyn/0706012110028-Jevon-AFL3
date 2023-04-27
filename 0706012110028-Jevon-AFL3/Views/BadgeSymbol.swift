1540f09321e7d84a00ab3f04ae8274558a9fbbfc
JevonCledwyn <jcledwyn@student.ciputra.ac.id>
2023-04-27 16:09:17 +0700

    Draw Paths and Shapes - section 1-3 (Create Drawing Data for a Badge View, Draw the Badge Background, Draw the Badge Symbol)

---
+++ 0706012110028-Jevon-AFL3/Views/BadgeSymbol.swift
@@ -0,0 +1,52 @@
+//
+//  BadgeSymbol.swift
+//  0706012110028-Jevon-AFL3
+//
+//  Created by MacBook Pro on 27/04/23.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    // Fill the symbol with the purple color
+    static let symbolColor = Color(red: 79.0 / 255, green: 79.0 / 255, blue: 191.0 / 255)
+
+    var body: some View {
+        // top portion of the symbol
+        GeometryReader { geometry in
+            Path { path in
+                let width = min(geometry.size.width, geometry.size.height)
+                let height = width * 0.75
+                let spacing = width * 0.030
+                let middle = width * 0.5
+                let topWidth = width * 0.226
+                let topHeight = height * 0.488
+
+                path.addLines([
+                    CGPoint(x: middle, y: spacing),
+                    CGPoint(x: middle - topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: spacing)
+                ])
+                
+                // bottom portion of the symbol
+                path.move(to: CGPoint(x: middle, y: topHeight / 2 + spacing * 3))
+                path.addLines([
+                    CGPoint(x: middle - topWidth, y: topHeight + spacing),
+                    CGPoint(x: spacing, y: height - spacing),
+                    CGPoint(x: width - spacing, y: height - spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight + spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing * 3)
+                ])
+            }
+            .fill(Self.symbolColor)
+        }
+    }
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
